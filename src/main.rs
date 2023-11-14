03ff8bf5210c0c96ad7b596deaac715671715600
Rithul Kamesh <hi@rithul.dev>
2023-11-14 21:08:24 +0530

    feat: move to rust

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,39 @@
 mod prisma;
-use axum::{response::Html, routing::get, Router};
-use log::info;
-use tokio::time::{Duration, Instant};
+use axum::{http::Response, routing::get, Router};
+use log::trace;
+use std::sync::{Arc, Mutex};
+use tokio::time::Duration;
 
 #[derive(Debug, Clone, Copy)]
 enum TimerState {
-    Running { start_time: Instant },
     Paused { remaining_time: Duration },
-    Stopped,
 }
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(handler));
+    let timer_state = Arc::new(Mutex::new(TimerState::Paused {
+        remaining_time: Duration::from_secs(25 * 60),
+    }));
+    let timer_state_clone = timer_state.clone();
 
-    info!("running on http://localhost:3000");
+    let app = Router::new().route(
+        "/status",
+        get(move || status_handler(timer_state_clone.clone())),
+    );
+
+    trace!("running on http://localhost:3000");
     axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
         .serve(app.into_make_service())
         .await
         .unwrap();
 }
 
-async fn handler() -> Html<&'static str> {
-    Html("<h1>Hello, World!</h1>")
+async fn status_handler(timer_state: Arc<Mutex<TimerState>>) -> Response<String> {
+    let state = timer_state.lock().unwrap();
+    let status = match *state {
+        TimerState::Paused { remaining_time } => {
+            format!("Paused, Remaining time: {:?}", remaining_time)
+        }
+    };
+    Response::new(status)
 }
