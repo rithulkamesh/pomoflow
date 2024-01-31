6e499f2278734197456f199e7ab4ec6154c59d4e
Rithul Kamesh <hi@rithul.dev>
2024-01-31 18:36:08 +0530

    fix: remove redundant timerType

--- chrome/src/app/session/[id]/page.tsx
+++ chrome/src/app/session/[id]/page.tsx
@@ -46,7 +46,6 @@ const SessionPage: React.FC<Props> = ({ params }) => {
   const [loading, setLoading] = useState<boolean>(true);
 
   const [completedSessions] = useState(0);
-  const [timerType] = useState(TimerType.Pomodoro);
   const [isHost, setIsHost] = useState(false);
   const [timeRemaining, setTimeRemaining] = useState(
     (session?.pomodoroTime ?? DEFAULTS.pomodoroTime) * 60
@@ -67,21 +66,36 @@ const SessionPage: React.FC<Props> = ({ params }) => {
   });
   const ref = doc(db, 'sessions', params.id);
 
+  const getTimeByType = (timerType: TimerType) => {
+    const { pomodoroTime, shortBreakTime, longBreakTime } = session ?? DEFAULTS;
+
+    const timeMapping: Record<TimerType, number> = {
+      [TimerType.Pomodoro]: pomodoroTime,
+      [TimerType.ShortBreak]: shortBreakTime,
+      [TimerType.LongBreak]: longBreakTime,
+    };
+
+    return timeMapping[timerType];
+  };
+
   useEffect(() => {
     if (!session || !session.isRunning || !session.startTime) return;
 
     const calculateTimeRemaining = () => {
       const now = Math.floor(Date.now() / 1000); // Current time in seconds
-      const { startTime, pausedTimes, pomodoroTime } = session;
+      const { startTime, pausedTimes, timerType } = session;
 
-      const remainingTime = pomodoroTime * 60 - (now - startTime);
-      const pausedTime = pausedTimes.reduce((acc, curr) => {
-        const { start, end } = curr;
-        if (end === null) {
-          return acc + (now - start);
-        }
-        return acc + (end - start);
-      }, 0);
+      const remainingTime = getTimeByType(timerType) * 60 - (now - startTime);
+      const pausedTime =
+        pausedTimes.length === 0
+          ? 0
+          : pausedTimes.reduce((acc, curr) => {
+              const { start, end } = curr;
+              if (end === null) {
+                return acc + (now - start);
+              }
+              return acc + (end - start);
+            }, 0);
 
       setTimeRemaining(remainingTime - pausedTime);
     };
@@ -90,11 +104,12 @@ const SessionPage: React.FC<Props> = ({ params }) => {
     const interval = setInterval(calculateTimeRemaining, 1000);
 
     return () => clearInterval(interval);
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [session]);
 
   const toggleTimer = () => {
     if (!session) return;
-
     const lastPause = session.pausedTimes.slice(-1)[0];
 
     const newPausedTimes =
@@ -117,14 +132,17 @@ const SessionPage: React.FC<Props> = ({ params }) => {
 
   const resetTimer = () => {
     if (!session) return;
-    const newSession: SessionDoc = {
+
+    const newSession = {
       ...session,
       startTime: Date.now(),
       pausedTimes: [],
       isRunning: false,
     };
+
+    setTimeRemaining(getTimeByType(newSession.timerType) * 60);
     setSession(newSession);
-    updateDoc(ref, newSession as any);
+    updateDoc(ref, newSession);
   };
 
   const updateTimer = (value: number, timerType: TimerType) => {
@@ -134,15 +152,26 @@ const SessionPage: React.FC<Props> = ({ params }) => {
   };
 
   const handleTimerTypeChange = (timerType: TimerType) => {
-    // Update the timer to a new timer type, and then update the session
-    console.error('Todo');
+    if (!session) return;
+    console.log(timerType);
+
+    const newSession = {
+      ...session,
+      timerType,
+      startTime: Date.now(),
+      pausedTimes: [],
+      isRunning: false,
+    };
+
+    setSession(newSession);
+    updateDoc(ref, newSession);
   };
 
   return (
     <main className='flex flex-col items-center justify-center p-24 gap-6 w-screen h-[calc(100vh-10rem)]'>
       <Timer
         loading={loading}
-        timerType={timerType}
+        timerType={session?.timerType as TimerType}
         isRunning={!!session?.isRunning}
         timeRemaining={timeRemaining}
         completedSessions={completedSessions}
