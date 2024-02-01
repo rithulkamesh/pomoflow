614cdc8646eab8a2797333e51fdfe12ffe6dfdad
Ahmed Haracic <me@qoobes.dev>
2024-02-01 09:47:26 +0100

    fix: utils.ts eslint

--- chrome/src/lib/utils.ts
+++ chrome/src/lib/utils.ts
@@ -7,7 +7,7 @@ export function cn(...inputs: ClassValue[]) {
 
 export function camelize(str: string) {
   return str
-    .replace(/(?:^\w|[A-Z]|\b\w)/g, function (word, index) {
+    .replace(/(?:^\w|[A-Z]|\b\w)/g, function(word, index) {
       return index === 0 ? word.toLowerCase() : word.toUpperCase();
     })
     .replace(/\s+/g, '');
@@ -16,5 +16,5 @@ export function camelize(str: string) {
 export function playAudio(url: string, volume: number) {
   const audio = new Audio(url);
   audio.volume = volume;
-  audio.play();
+  void audio.play();
 }
