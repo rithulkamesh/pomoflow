c750d9f441f80508192211d9293fda979e8ca5ef
Ahmed Haracic <me@qoobes.dev>
2024-02-01 09:43:00 +0100

    feat: add time helpers

---
+++ chrome/src/lib/time.ts
@@ -0,0 +1,40 @@
+import { TimerType } from '@/components/dash/timer';
+import { SessionDoc } from '@/components/sessions/SessionPage';
+
+export const getTimeByType = (timerType: TimerType, session?: SessionDoc) => {
+  if (!session) return 0;
+
+  const { pomodoroTime, shortBreakTime, longBreakTime } = session;
+
+  const timeMapping: Record<TimerType, number> = {
+    [TimerType.Pomodoro]: pomodoroTime,
+    [TimerType.ShortBreak]: shortBreakTime,
+    [TimerType.LongBreak]: longBreakTime,
+  };
+
+  return timeMapping[timerType];
+};
+
+export const calculateTimeRemaining = (session: SessionDoc) => {
+  if (!session.isRunning || !session.startTime) return;
+
+  const now = Date.now(); // Current time in miliseconds
+  const { startTime, pausedTimes, timerType } = session;
+
+  const sessionDuration = getTimeByType(timerType, session) * 60 * 1000;
+  const elapsedTime = now - startTime;
+
+  const pausedTime =
+    pausedTimes.length === 0
+      ? 0
+      : pausedTimes.reduce((acc, curr) => {
+          const { start, end } = curr;
+          if (end === null) {
+            return acc + (now - start);
+          }
+          return acc + (end - start);
+        }, 0);
+
+  const remainingTime = sessionDuration - elapsedTime + pausedTime;
+  return Math.floor(remainingTime / 1000);
+};
