0a1ed3654820a355e84188a54461051cb86f85d7
Ahmed Haracic <me@qoobes.dev>
2024-02-11 17:51:22 +0100

    feat: update NotionAvatar to pass through className

--- web/src/components/NotionAvatar.tsx
+++ web/src/components/NotionAvatar.tsx
@@ -2,18 +2,23 @@ import { notionists } from '@dicebear/collection';
 import { createAvatar } from '@dicebear/core';
 import React from 'react';
 import { Avatar, AvatarFallback, AvatarImage } from './ui/avatar';
+import { cn } from '@/lib/utils';
 
 interface Props {
   name: string;
+  className?: string;
 }
 
-const NotionAvatar: React.FC<Props> = ({ name }) => {
+const NotionAvatar: React.FC<Props> = ({ name, className }) => {
   const avatar = createAvatar(notionists, {
     seed: name,
   });
   return (
     <Avatar>
-      <AvatarImage src={avatar.toDataUriSync()} className='dark:bg-white' />
+      <AvatarImage
+        src={avatar.toDataUriSync()}
+        className={cn('dark:bg-white', className)}
+      />
       <AvatarFallback>{name}</AvatarFallback>
     </Avatar>
   );
