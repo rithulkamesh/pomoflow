7bb05b3a6dbdd3221b3a1ab33a7685d5ff958086
Rithul Kamesh <hi@rithul.dev>
2024-02-16 19:23:30 +0530

    feat: blogs update

---
+++ mobile/metro.config.js
@@ -0,0 +1,8 @@
+// Learn more https://docs.expo.io/guides/customizing-metro
+const { getDefaultConfig } = require("expo/metro-config");
+
+/** @type {import('expo/metro-config').MetroConfig} */
+const config = getDefaultConfig(__dirname);
+config.resolver.sourceExts.push("cjs");
+
+module.exports = config;
---
+++ mobile/src/lib/firebase.ts
@@ -0,0 +1,19 @@
+"use client";
+
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC2RnLU6JuD8laqwhwKz81gYRxMChuu_es",
+  authDomain: "pomotimer-fe5c2.firebaseapp.com",
+  projectId: "pomotimer-fe5c2",
+  storageBucket: "pomotimer-fe5c2.appspot.com",
+  messagingSenderId: "181805257103",
+  appId: "1:181805257103:web:6e8cd9127a6c22274d7bfa",
+  measurementId: "G-WWXJ6D4W6B",
+};
+
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
