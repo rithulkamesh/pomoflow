146fdebfd49ea99ac50cf64d750debfbad853443
Rithul Kamesh <hi@rithul.dev>
2024-02-17 18:14:47 +0530

    feat: update meta tags

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -16,6 +16,30 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <meta
+        name="description"
+        content="A distraction-free collaborative pomodoro timer."
+      />
+
+      <meta property="og:url" content="https://pomoflow.io" />
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content="Pomoflow" />
+      <meta
+        property="og:description"
+        content="A distraction-free collaborative pomodoro timer."
+      />
+      <meta property="og:image" content="/banner_sm.png" />
+
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta property="twitter:domain" content="pomoflow.io" />
+      <meta property="twitter:url" content="https://pomoflow.io" />
+      <meta name="twitter:title" content="Pomoflow" />
+      <meta
+        name="twitter:description"
+        content="A distraction-free collaborative pomodoro timer."
+      />
+      <meta name="twitter:image" content="/banner_sm.png" />
+
       <body className={poppins.className}>
         <ThemeProvider attribute="class" defaultTheme="dark">
           {children}
